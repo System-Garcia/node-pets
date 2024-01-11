8cbc9fc37e9ff9172732dc9074c80acbad0c61f8
Axel Adrian Garcia Aldama <a22311061@uthermosillo.edu.mx>
2024-01-11 16:25:59 -0700

    Actualizar .env.template

--- prisma/migrations/20240105032556_corregir/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `missingAt` on table `Pet` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE "Pet" ALTER COLUMN "missingAt" SET NOT NULL;
