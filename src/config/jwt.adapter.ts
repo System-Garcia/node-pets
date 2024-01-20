8004484b578857a8f53ff5767f948b073446dd9b
Axel Adrian Garcia Aldama <a22311061@uthermosillo.edu.mx>
2024-01-19 22:58:36 -0700

    jwt.adpater.ts creado

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,5 +1,35 @@
-import jws from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export class JwtGeneraton {
-    
+
+  constructor(private readonly jwtSeed: string) {}
+
+  public async generateToken(payload: any, duration: string = "2h") {
+
+    return new Promise((resolve) => {
+
+      jwt.sign(payload, this.jwtSeed, { expiresIn: duration }, (err, token) => {
+
+        if (err) return resolve(null);
+
+        resolve(token);
+      });
+    });
+  };
+
+  public async validateToken<T>(token: string): Promise<T | null> {
+
+    return new Promise( (resolve) => {
+
+        jwt.verify(token, this.jwtSeed, (err, decoded) => {
+
+            if(err) return resolve (null);
+
+            resolve(decoded as T);
+        });
+
+    });
+
+  }
+
 }
